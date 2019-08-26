c40b191f6a628b21caa9f46eb562f96c52c4c70f
Mathieu MARACHE <mathieu.marache@ibm.com>
2019-08-26 10:26:55 +0200

    Added setup

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+     name='changelog',  
+     version='1.1.0',
+     scripts=['changelog'] ,
+     author="Mathieu MARACHE",
+     author_email="mathieu@marache.com",
+     description="A changelog update cli tool",
+     long_description=long_description,
+     long_description_content_type="text/markdown",
+     url="https://github.com/mathieu/changelog-cli",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
