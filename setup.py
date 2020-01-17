4e7424f587d9b80d902b452be4a6b486f9ef9bdb
Mathieu MARACHE <mathieu.marache@ibm.com>
2020-01-17 17:12:07 +0100

    releasing 1.2.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
      name='cli-changelog',  
-     version='1.1.1',
+     version='1.2.0',
      scripts=['changelog'] ,
      author="Mathieu MARACHE",
      author_email="mathieu@marache.com",
